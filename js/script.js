198ae879655235d53f41d57c11b8a30bd34c3e7c
manoj876 <mirgemanoj17@gmail.com>
2020-08-15 19:34:00 +0530

    nd

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
-function manoj2(){var a=document.getElementsByTagName("a");
-	var b =document.getElementById("searcj").value.toUppperCase();
-	for (var i = 0; i <a.length; i++) {if (a[i].innerHTML.toUppperCase().indexOf(b)>-1){
+function man(){var a=document.getElementsByTagName("a");
+	var b =document.getElementById("searcj").value.toUpperCase();
+	for (var i = 0; i <a.length; i++) {if (a[i].innerHTML.toUpperCase().indexOf(b)>-1){
 		a[i].style.display= "";
 	}	
 	else {a[i].style.display= "none";}
@@ -17,8 +17,9 @@ document.getElementById("m1").classList.remove("main2");
 
 
 
-function manoj1() { document.getElementById("sidenav").classList.add("sidebar2");
+function mano() { document.getElementById("sidenav").classList.add("sidebar2");
 document.getElementById("m1").classList.add("main2");
 
 
 }
+console.log(document.getElementsByTagName("a"));
