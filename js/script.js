052e6f4ebd7358558c76935d5839617291ff1566
manoj876 <mirgemanoj17@gmail.com>
2020-08-15 13:40:28 +0530

    man

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 
-
+function manoj2(){var a=document.getElementsByTagName("a");
+	var b =document.getElementById("searcj").value.toUppperCase();
+	for (var i = 0; i <a.length; i++) {if (a[i].innerHTML.toUppperCase().indexOf(b)>-1){
+		a[i].style.display= "";
+	}	
+	else {a[i].style.display= "none";}
+	}
+}
 
 
 function manoj() { document.getElementById("sidenav").classList.remove("sidebar2");
 document.getElementById("m1").classList.remove("main2");
-};
+}
 
 
 
@@ -14,19 +21,4 @@ function manoj1() { document.getElementById("sidenav").classList.add("sidebar2")
 document.getElementById("m1").classList.add("main2");
 
 
-};
-function manoj2(){var a=document.getElementsByTagName('a');
-	var b =document.getElementByTagName("input").value.toUppperCase();
-	for (var i = 0; i <a.length; i++;) {
-
-	if (a[i].innerHTML.toUppperCase().indexOf(b)>-1;){
-		a[i].style.display= "";
-	};	
-	else {a[i].style.display= "none";};
-	};
-
-
-
-
-
-};
+}
